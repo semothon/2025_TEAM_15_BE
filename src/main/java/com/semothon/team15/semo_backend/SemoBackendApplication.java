f59054eafd4c85f0572541d4b4b23fd977a5c933
kmin1231 <kmin91143@gmail.com>
2025-03-25 21:46:15 +0900

    🌱 chore: #1 initialize the spring boot project

---
+++ src/main/java/com/semothon/team15/semo_backend/SemoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.semothon.team15.semo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semothon/team15/semo_backend/SemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semothon.team15.semo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
